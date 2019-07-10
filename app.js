a4460338dec4f5eb6de47ed6fe8a28ae8132853a
Souvik Kundu <kivtech88@gmail.com>
2019-07-10 14:36:12 +0530

    commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 app.get('/', function(request,response) {
     response.sendFile(__dirname+'/index.html');
 });
-var port = 3000;
+var port = process.env.PORT || 8080;
 var server=app.listen(port, function(req,res) {
     console.log("Catch the action at http://localhost:"+port);
 });
